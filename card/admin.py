c013ee27d3599157c2475cd1aa933b7e51fd3018
murgom <murgom@naver.com>
2018-01-23 17:10:26 +0900

    makemigrations 갱신

--- card/admin.py
+++ card/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from card.models import Post, PostGallery ,Comment, SmsImage
+from card.models import Post, PostGallery ,Comment, SmsImage, SamplePost, SamplePostGallery ,SampleComment, SampleSmsImage
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
@@ -46,44 +46,44 @@ class PostNotCommentAdmin(SummernoteModelAdmin):
     list_display = ['id', 'name','message_image1','created_at', 'updated_at']
 
 
-# @admin.register(SamplePost)
-# class PostAdmin(SummernoteModelAdmin):
-#     list_display = ['id', 'title','post_content_str','main_image1','created_at', 'updated_at']
-#     list_display_links = ('title',)
+@admin.register(SamplePost)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ['id', 'title','post_content_str','main_image1','created_at', 'updated_at']
+    list_display_links = ('title',)
 
-#     def post_content_str(self, post):
-#         return '(샘플)청첩장 이름(댓글O) : {}'.format(str(post.title))
+    def post_content_str(self, post):
+        return '(샘플)청첩장 이름(댓글O) : {}'.format(str(post.title))
 
-# @admin.register(SamplePostGallery)
-# class PostNotCommentAdmin(SummernoteModelAdmin):
-#     list_display = ['id', 'name','post_content_str','created_at', 'updated_at']
-#     list_display_links = ('name',)
+@admin.register(SamplePostGallery)
+class PostNotCommentAdmin(SummernoteModelAdmin):
+    list_display = ['id', 'name','post_content_str','created_at', 'updated_at']
+    list_display_links = ('name',)
 
-#     def post_content_str(self, post):
-#         return '(샘플)연결된 청첩장(댓글O) : {}'.format(str(post.post))
+    def post_content_str(self, post):
+        return '(샘플)연결된 청첩장(댓글O) : {}'.format(str(post.post))
 
-# @admin.register(SampleComment)
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'post_title_str','post_user_str','post_message_str','password1','comment_content_len']
-#     list_display_links = ('post_user_str','post_title_str')
-#     # prepopulated_fields = { 'password2': ['password1'] }
+@admin.register(SampleComment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post_title_str','post_user_str','post_message_str','password1','comment_content_len']
+    list_display_links = ('post_user_str','post_title_str')
+    # prepopulated_fields = { 'password2': ['password1'] }
 
-#     def post_user_str(self,comment):
-#         return '(샘플)작성자 : {}'.format(str(comment.user))
+    def post_user_str(self,comment):
+        return '(샘플)작성자 : {}'.format(str(comment.user))
 
-#     def post_message_str(self,comment):
-#         return '(샘플)댓글 내용 : {}'.format(str(comment.message))
+    def post_message_str(self,comment):
+        return '(샘플)댓글 내용 : {}'.format(str(comment.message))
 
-#     def post_title_str(self,comment):
-#         return '(샘플){} 게시물 댓글'.format(str(comment.post.title))
+    def post_title_str(self,comment):
+        return '(샘플){} 게시물 댓글'.format(str(comment.post.title))
 
-#     def comment_content_len(self,comment):
-#         return '(샘플)댓글 : {}글자'.format(len(comment.message))
+    def comment_content_len(self,comment):
+        return '(샘플)댓글 : {}글자'.format(len(comment.message))
 
-#     def get_queryset(self,request):
-#         qs = super().get_queryset(request)
-#         return qs.select_related('post')
+    def get_queryset(self,request):
+        qs = super().get_queryset(request)
+        return qs.select_related('post')
 
-# @admin.register(SampleSmsImage)
-# class PostNotCommentAdmin(SummernoteModelAdmin):
-#     list_display = ['id', 'name','message_image1','created_at', 'updated_at']
+@admin.register(SampleSmsImage)
+class PostNotCommentAdmin(SummernoteModelAdmin):
+    list_display = ['id', 'name','message_image1','created_at', 'updated_at']
--- card/forms.py
+++ card/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Post,Comment
+from .models import Post,Comment,SamplePost,SampleComment
 from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
 from django.utils.translation import ugettext, ugettext_lazy as _
@@ -44,40 +44,40 @@ class CommentDeleteForm(forms.ModelForm):
             )
         return password2
 
-# class SamplePostForm(forms.ModelForm):
-#     title = forms.CharField(max_length=100)
-#     youtube_url = forms.CharField(widget=SummernoteWidget(attrs={'width': '100%', 'src':'img-responsive img-rounded'} ))
+class SamplePostForm(forms.ModelForm):
+    title = forms.CharField(max_length=100)
+    youtube_url = forms.CharField(widget=SummernoteWidget(attrs={'width': '100%', 'src':'img-responsive img-rounded'} ))
 
-#     class Meta:
-#         model = SamplePost
-#         fields = ('title','youtube_url',)
-
-
-
-# class SampleCommentForm(forms.ModelForm):
-
-#     class Meta:
-#         model = SampleComment
-#         fields = ('sample_author','user','password1','message',)
+    class Meta:
+        model = SamplePost
+        fields = ('title','youtube_url',)
 
 
-# class SampleCommentDeleteForm(forms.ModelForm):
-#     error_messages = {
-#         'password_mismatch': _("The two password fields didn't match."),
-#     }
-#     password1 = forms.CharField(widget=forms.PasswordInput)
-#     password2 = forms.CharField(widget=forms.PasswordInput)
 
-#     class Meta:
-#         model = SampleComment
-#         fields = ('sample_author','user','password1','message',)
+class SampleCommentForm(forms.ModelForm):
 
-#     def clean_password2(self):
-#         password1 = self.cleaned_data.get("password1")
-#         password2 = self.cleaned_data.get("password2")
-#         if password1 and password2 and password1 != password2:
-#             raise forms.ValidationError(
-#                 self.error_messages['password_mismatch'],
-#                 code='password_mismatch',
-#             )
-#         return password2
+    class Meta:
+        model = SampleComment
+        fields = ('author','user','password1','message',)
+
+
+class SampleCommentDeleteForm(forms.ModelForm):
+    error_messages = {
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = SampleComment
+        fields = ('author','user','password1','message',)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
